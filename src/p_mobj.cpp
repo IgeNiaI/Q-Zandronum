41068aec21f49687f39ad7b51d5163dcced59a99
geNia <geniann93@gmail.com>
2020-11-14 23:20:50 -0500

    Fixed crash when starting offline skirmish while in multiplayer game

--- src/p_mobj.cpp
+++ src/p_mobj.cpp
@@ -2087,8 +2087,11 @@ fixed_t P_XYMovement (AActor *mo, fixed_t scrollx, fixed_t scrolly)
 	// that large thrusts can't propel an actor through a wall, because wall
 	// running depends on the player's original movement continuing even after
 	// it gets blocked.
-	if (!quakeMovement && (mo->player != NULL && (i_compatflags & COMPATF_WALLRUN)) || (mo->waterlevel >= 1) ||
-		(mo->player != NULL && mo->player->crouchfactor < mo->player->mo->CrouchScaleHalfWay))
+	if (!quakeMovement
+		&& ((mo->player && (i_compatflags & COMPATF_WALLRUN))
+		|| (mo->waterlevel >= 1)
+		|| (mo->player && mo->player->mo
+			&& mo->player->crouchfactor < mo->player->mo->CrouchScaleHalfWay)))
 	{
 		// preserve the direction instead of clamping x and y independently.
 		xmove = clamp (mo->velx, -maxmove, maxmove);
