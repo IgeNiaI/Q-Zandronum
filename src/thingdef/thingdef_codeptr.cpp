4df78aa02dce50296b1612ae4d0c1370a285675d
Evghenii Olenciuc <9607628+igeniai@users.noreply.github.com>
2020-12-18 16:29:29 -0500

    Replaced quake thrust flag with global cvar

--- src/thingdef/thingdef_codeptr.cpp
+++ src/thingdef/thingdef_codeptr.cpp
@@ -1428,7 +1428,6 @@ enum FB_Flags
 	FBF_NOPITCH = 8,
 	FBF_NOFLASH = 16,
 	FBF_NORANDOMPUFFZ = 32,
-	FBF_QUAKETHRUST = 64,
 };
 
 // [BB] This functions is needed to keep code duplication at a minimum while applying the spread power.
@@ -1508,8 +1507,6 @@ void A_CustomFireBullets( AActor *self,
 	int bangle;
 	int bslope = 0;
 	int laflags = Flags & FBF_NORANDOMPUFFZ ? LAF_NORANDOMPUFFZ : 0;
-	if (Flags & FBF_QUAKETHRUST)
-		laflags |= LAF_QUAKETHRUST;
 
 	if ((Flags & FBF_USEAMMO) && weapon && !weapon->DepleteAmmo(weapon->bAltFire, true)) // out of ammo
 		return;
