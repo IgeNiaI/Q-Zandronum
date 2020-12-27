9f59fbbd1b64495a322ddda2a62dc04f60de917b
Evghenii Olenciuc <9607628+IgeNiaI@users.noreply.github.com>
2020-12-27 13:41:00 -0500

    Fixed footsteps not playing properly in doom movement

--- src/d_player.h
+++ src/d_player.h
@@ -111,7 +111,7 @@ public:
 	virtual void DropImportantItems( bool bLeavingGame, AActor *pSource = NULL );
 
 	virtual int WalkCrouchState ();
-	virtual bool ShouldPlayFootstep ();
+	virtual void PlayFootsteps ();
 	virtual void TweakSpeeds (int &forwardmove, int &sidemove);
 	virtual void MorphPlayerThink ();
 	virtual void ActivateMorphWeapon ();
