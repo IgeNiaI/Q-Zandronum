4fe87919bf463fa12f6058e30aca234fa0ec5f2f
Evghenii Olenciuc <9607628+IgeNiaI@users.noreply.github.com>
2021-01-28 00:55:51 -0500

    (Leo) Fixed: use the correct values for TicStart and TicNext with the windows polling timer

--- src/win32/i_system.cpp
+++ src/win32/i_system.cpp
@@ -347,8 +347,9 @@ static int I_GetTimePolled(bool saveMS)
 	}
 	if (saveMS)
 	{
-		TicStart = tm;
-		TicNext = (tm * TICRATE / 1000 + 1) * 1000 / TICRATE;
+		DWORD CurrentTic = ((tm - basetime) * TICRATE) / 1000;
+		TicStart = (CurrentTic * 1000 / TICRATE) + basetime;
+		TicNext = ((CurrentTic + 1) * 1000 / TICRATE) + basetime;
 	}
 
 	return ((tm-basetime)*TICRATE)/1000;
