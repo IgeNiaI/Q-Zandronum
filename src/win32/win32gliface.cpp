5cb113a95a815af6acbce8d32222db9635ddc3e0
Evghenii Olenciuc <9607628+IgeNiaI@users.noreply.github.com>
2021-03-07 17:33:56 -0500

    Add vid_maxfps support for Windows and Linux

--- src/win32/win32gliface.cpp
+++ src/win32/win32gliface.cpp
@@ -1060,6 +1060,11 @@ void Win32GLFrameBuffer::SetVSync (bool vsync)
 
 void Win32GLFrameBuffer::SwapBuffers()
 {
+	// Limiting the frame rate is as simple as waiting for the timer to signal this event.
+	if (FPSLimitEvent != NULL)
+	{
+		WaitForSingleObject(FPSLimitEvent, 1000);
+	}
 	::SwapBuffers(static_cast<Win32GLVideo *>(Video)->m_hDC);
 }
 
