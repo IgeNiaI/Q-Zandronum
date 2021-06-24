edab31c072f8517a244d884a21e26d1c070c006b
Evghenii Olenciuc <9607628+igeniai@users.noreply.github.com>
2021-06-24 16:59:04 -0400

    Unlagged Doom weapons

--- src/g_doom/a_doomweaps.cpp
+++ src/g_doom/a_doomweaps.cpp
@@ -34,7 +34,8 @@ DEFINE_ACTION_FUNCTION(AActor, A_Punch)
 	AActor		*linetarget;
 
 	// [BC] Weapons are handled by the server.
-	if ( NETWORK_InClientMode() )
+	// [geNia] Unless clientside functions are allowed.
+	if ( !NETWORK_ClientsideFunctionsAllowedOrIsServer( self ) )
 	{
 		return;
 	}
@@ -83,18 +84,17 @@ DEFINE_ACTION_FUNCTION(AActor, A_Punch)
 	// turn to face target
 	if (linetarget)
 	{
-		S_Sound (self, CHAN_WEAPON, "*fist", 1, ATTN_NORM);
+		S_Sound (self, CHAN_WEAPON, "*fist", 1, ATTN_NORM, true);
 		self->angle = R_PointToAngle2 (self->x,
 										self->y,
 										linetarget->x,
 										linetarget->y);
 
-		// [BC] Play the hit sound to clients.
 		if ( NETWORK_GetState( ) == NETSTATE_SERVER )
-		{
-			SERVERCOMMANDS_SoundActor( self, CHAN_WEAPON, "*fist", 1, ATTN_NORM );
-			SERVERCOMMANDS_SetThingAngle( self );
-		}
+			if ( NETWORK_ClientsideFunctionsAllowed( self ) )
+				SERVERCOMMANDS_SetThingAngle( self, ULONG( self->player - players ), SVCF_SKIPTHISCLIENT );
+			else
+				SERVERCOMMANDS_SetThingAngle( self );
 	}
 }
 
@@ -208,7 +208,8 @@ DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_Saw)
 	int actualdamage;
 
 	// [BC] Weapons are handled by the server.
-	if ( NETWORK_InClientMode() )
+	// [geNia] Unless clientside functions are allowed.
+	if ( !NETWORK_ClientsideFunctionsAllowedOrIsServer( self ) )
 	{
 		return;
 	}
@@ -281,11 +282,7 @@ DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_Saw)
 		{
 			player->extralight = !player->extralight;
 		}
-		S_Sound (self, CHAN_WEAPON, fullsound, 1, ATTN_NORM);
-
-		// [BC] If we're the server, tell clients to play the saw sound.
-		if ( NETWORK_GetState( ) == NETSTATE_SERVER )
-			SERVERCOMMANDS_SoundActor( self, CHAN_WEAPON, S_GetName( fullsound ), 1, ATTN_NORM );
+		S_Sound (self, CHAN_WEAPON, fullsound, 1, ATTN_NORM, true);
 		return;
 	}
 
@@ -316,10 +313,7 @@ DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_Saw)
 	if ( ( NETWORK_GetState() == NETSTATE_SERVER ) && self->player && prevhealth != self->health )
 		SERVERCOMMANDS_SetPlayerHealth( self->player - players );
 
-	S_Sound (self, CHAN_WEAPON, hitsound, 1, ATTN_NORM);
-	// [BC] If we're the server, tell clients to play the saw sound.
-	if ( NETWORK_GetState( ) == NETSTATE_SERVER )
-		SERVERCOMMANDS_SoundActor( self, CHAN_WEAPON, S_GetName( hitsound ), 1, ATTN_NORM );
+	S_Sound (self, CHAN_WEAPON, hitsound, 1, ATTN_NORM, true);
 		
 	// turn to face target
 	angle = R_PointToAngle2 (self->x, self->y,
@@ -342,7 +336,10 @@ DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_Saw)
 
 	// [BC] If we're the server, tell clients to adjust the player's angle.
 	if ( NETWORK_GetState( ) == NETSTATE_SERVER )
-		SERVERCOMMANDS_SetThingAngle( self );
+		if ( NETWORK_ClientsideFunctionsAllowed( self ) )
+			SERVERCOMMANDS_SetThingAngle( self, ULONG( self->player - players ), SVCF_SKIPTHISCLIENT );
+		else
+			SERVERCOMMANDS_SetThingAngle( self );
 
 	// [BC] Tell all the bots that a weapon was fired.
 	BOTS_PostWeaponFiredEvent( ULONG( self->player - players ), BOTEVENT_USEDCHAINSAW, BOTEVENT_ENEMY_USEDCHAINSAW, BOTEVENT_PLAYER_USEDCHAINSAW );
@@ -734,7 +731,8 @@ DEFINE_ACTION_FUNCTION(AActor, A_FireMissile)
 	}
 
 	// [BC] Weapons are handled by the server.
-	if ( NETWORK_InClientMode() )
+	// [geNia] Unless clientside functions are allowed.
+	if ( !NETWORK_ClientsideFunctionsAllowedOrIsServer( self ) )
 	{
 		return;
 	}
@@ -802,7 +800,8 @@ DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_FireSTGrenade)
 	}
 		
 	// Weapons are handled by the server.
-	if ( NETWORK_InClientMode() )
+	// [geNia] Unless clientside functions are allowed.
+	if ( !NETWORK_ClientsideFunctionsAllowedOrIsServer( self ) )
 	{
 		return;
 	}
@@ -842,7 +841,8 @@ DEFINE_ACTION_FUNCTION(AActor, A_FirePlasma)
 	}
 
 	// [BC] Weapons are handled by the server.
-	if ( NETWORK_InClientMode() )
+	// [geNia] Unless clientside functions are allowed.
+	if ( !NETWORK_ClientsideFunctionsAllowedOrIsServer( self ) )
 	{
 		return;
 	}
@@ -881,8 +881,9 @@ static void FireRailgun(AActor *self, int offset_xy, const PClass* puffType = NU
 	}
 
 	// Weapons are handled by the server.
+	// [geNia] Unless clientside functions are allowed.
 	// [Spleen] But railgun is an exception if it's unlagged, to make it look nicer
-	if ( NETWORK_InClientMode()
+	if ( !NETWORK_ClientsideFunctionsAllowedOrIsServer( self )
 		&& !UNLAGGED_DrawRailClientside( self ) )
 	{
 		return;
@@ -967,7 +968,8 @@ DEFINE_ACTION_FUNCTION(AActor, A_FireBFG)
 	}
 
 	// [BC] Weapons are handled by the server.
-	if ( NETWORK_InClientMode() )
+	// [geNia] Unless clientside functions are allowed.
+	if ( !NETWORK_ClientsideFunctionsAllowedOrIsServer( self ) )
 	{
 		return;
 	}
@@ -998,7 +1000,7 @@ DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_BFGSpray)
 	AActor				*linetarget;
 
 	// [BC] This is not done on the client end.
-	if ( NETWORK_InClientMode() )
+	if ( !NETWORK_ClientsideFunctionsAllowedOrIsServer( self ) )
 	{
 		return;
 	}
