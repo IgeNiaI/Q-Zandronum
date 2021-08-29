16d740a4c8e2d2cda478b391fbd7464b0e16c8d4
Evghenii Olenciuc <9607628+igeniai@users.noreply.github.com>
2021-08-29 01:44:52 -0400

    Brought back old fly/swim formula, refactored the new formula and added compat_newflyformula cvar to enable it

--- src/d_main.cpp
+++ src/d_main.cpp
@@ -610,6 +610,7 @@ CVAR (Flag, sv_noaccuratecrosshair,			zadmflags, ZADF_DISABLE_CROSSHAIR_ACCURATE
 CVAR (Flag, sv_disablewallfriction,			zadmflags, ZADF_DISABLE_WALL_FRICTION);
 CVAR (Flag, sv_enableprojectilehitboxfix,	zadmflags, ZADF_ENABLE_PROJECTILE_HITBOX_FIX);
 CVAR (Flag, sv_quakethrust,					zadmflags, ZADF_QUAKE_THRUST);
+CVAR (Flag, sv_newflyformula,				zadmflags, ZADF_NEW_FLY_FORMULA);
 
 //==========================================================================
 //
