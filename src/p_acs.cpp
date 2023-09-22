ce03605562720a165e0dfb62b8103cef5ea554e4
Evghenii <geniann93@gmail.com>
2023-09-22 16:29:08 -0400

    Added `InCampaign()` ACS function

--- src/p_acs.cpp
+++ src/p_acs.cpp
@@ -90,6 +90,7 @@
 #include "cl_commands.h"
 #include "cl_main.h"
 #include "unlagged.h"
+#include "campaign.h"
 
 #include "g_shared/a_pickups.h"
 
@@ -5727,6 +5728,7 @@ enum EACSFunctions
 	ACSF_UnlaggedReconcile,
 	ACSF_UnlaggedRestore,
 	ACSF_SyncPlayerNetwork,
+	ACSF_InCampaign,
 
 	ACSF_LumpOpen = 159, // [TDRR] Added the LumpOpen to LumpClose set of functions.
 	ACSF_LumpRead,
@@ -8540,6 +8542,11 @@ doplaysound:			if (funcIndex == ACSF_PlayActorSound)
 					ACSLumpHandles.Remove( args[0] );
 
 				return 0;
+			}
+			
+		case ACSF_InCampaign:
+			{
+				return CAMPAIGN_InCampaign();
 			}
 
 		default:
