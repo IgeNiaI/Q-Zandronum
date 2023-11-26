b91983569f31af9d2c8918498066f25523a42a65
Evghenii <geniann93@gmail.com>
2023-11-26 18:04:04 -0500

    Updated issues url text

--- src/win32/i_crash.cpp
+++ src/win32/i_crash.cpp
@@ -1977,7 +1977,7 @@ static INT_PTR CALLBACK OverviewDlgProc (HWND hDlg, UINT message, WPARAM wParam,
 		SendMessage (edit, WM_SETTEXT, 0, (LPARAM)"Please tell us about this problem.\n"
 			"The information will NOT be sent to Microsoft.\n\n"
 			"An error report has been created that you can submit to help improve " GAMENAME ". "
-			"You can either save it to disk and make a report in the QC:DE discord at " DISCORD_URL ", "
+			"You can either save it to disk and make a report at GitHub at " BUGS_URL ", "
 			"or you can send it directly without letting other people know about it.");
 		SendMessage (edit, EM_SETSEL, 0, 81);
 		SendMessage (edit, EM_SETCHARFORMAT, SCF_SELECTION, (LPARAM)&charFormat);
